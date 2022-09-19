264b240b5e6debcd5b5216462fe2ef3e19243141
ValentG2 <valentin.gregoire@basf.com>
2022-09-19 16:50:56 +0200

    some stuff:

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -113,7 +113,7 @@ return packer.startup(function(use)
 
 	-- snippets
 	use({ "L3MON4D3/LuaSnip" }) --snippet engine
-	use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
+	--[[ use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use ]]
 
 	-- Telescope
 	use({ "nvim-telescope/telescope.nvim" })
